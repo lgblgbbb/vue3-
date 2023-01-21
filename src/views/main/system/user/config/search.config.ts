8a9dad8d11da7c4f95947544194fb20468cfb55b
xiaoli <1184912869@qq.com>
2023-01-21 16:42:13 +0800

    实现form双向绑定

--- src/views/main/system/user/config/search.config.ts
+++ src/views/main/system/user/config/search.config.ts
@@ -2,29 +2,33 @@ import { IForm } from '@/base-ui/form'
 
 export const searchFormConfig: IForm = {
   labelWidth: '120px',
-  itemLayout: {
-    padding: '10px 40px'
+  itemStyle: {
+    padding: '10px 20px'
   },
   colLayout: {
     span: 8
   },
   formItems: [
     {
+      filed: 'id',
       type: 'input',
       label: 'id',
       placeholder: '请输入id'
     },
     {
+      filed: 'input',
       type: 'input',
       label: '用户名',
       placeholder: '请输入用户名'
     },
     {
+      filed: 'password',
       type: 'password',
       label: '密码',
       placeholder: '请输入密码'
     },
     {
+      filed: 'sport',
       type: 'select',
       label: '喜欢的运动',
       placeholder: '请选择喜欢的运动',
@@ -34,6 +38,7 @@ export const searchFormConfig: IForm = {
       ]
     },
     {
+      filed: 'createTime',
       type: 'datepicker',
       label: '创建时间',
       otherOptions: {
